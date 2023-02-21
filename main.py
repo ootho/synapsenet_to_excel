7b5fbcfe2d0e3ce9cc21f93a5b345f03d646e4ce
ootho <dds@DESKTOP-OCLV7S6.localdomain>
2023-02-22 00:08:08 +0300

    Add tax_id reciever, add responce test-dummy, add write xlsx-dummy

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+import files_io
+import xlsxwriter
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/tax_id/", methods=['POST'])
+def tax_id():
+    data = request.get_json()
+    path = 'file.txt'
+    files_io.write_json_array(data, path)
+    files_io.timestamp()
+    return "Tax id received!"
+
+@app.route("/synapse_responce/", methods=['POST'])
+def synapse_responce():
+    data = request.get_json()
+    file_name = data['ИНН']
+    path = str(f'temp_data/{file_name}')
+    files_io.write_json_array(data, path)
+    return "Json received succesfully!"
+
+@app.route("/write_xlsx/", methods=['POST'])
+def write_xlsx():
+    files_io.write_xlsx()
+    return "Write is done!"
